f6f6e94d2c67c2f2d9c6124489dc7c40405736b7
Brenda Esmerio <azucenaesmerio@gmail.com>
2022-10-06 11:47:48 -0500

    Initial commit

---
+++ app/src/main/java/com/example/brendacriminalintent/crime.kt
@@ -0,0 +1,11 @@
+package com.example.brendacriminalintent
+
+import java.util.Date
+import java.util.UUID
+
+class crime {
+    val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+}
